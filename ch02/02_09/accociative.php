2088476ecd9f51227f1e546f645d11b919f5b790
Bret <Burhooo@gmail.com>
2018-12-07 05:31:49 -0600

    completed challenge 02/10

---
+++ ch02/02_09/accociative.php
@@ -0,0 +1,13 @@
+<?php
+$descriptions = [
+    'Earth' => 'mostly harmless',
+    'Marvin' => 'paranoid android'
+];
+
+$descriptions['Zaphod'] = 'President of the imperial galactic government';
+
+// echo $descriptions['Marvin'];
+
+// print_r($descriptions);
+
+echo "Earth is described as {$descriptions['Earth']}";
