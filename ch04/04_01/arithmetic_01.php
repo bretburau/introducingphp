5790f9afa5ad5507ca2b6b24f4f5f8fcd0ea358d
Bret <Burhooo@gmail.com>
2018-12-09 05:36:24 -0600

    finished 04-01

--- ch04/04_01/arithmetic_01.php
+++ ch04/04_01/arithmetic_01.php
@@ -2,9 +2,12 @@
 $a = 5;
 $b = 2;
 
-echo -$a . '<br>';
+echo -   $a . '<br>';
 echo $a + $b . '<br>';
 echo $a - $b . '<br>';
 echo $a * $b . '<br>';
 echo $a / $b . '<br>';
 
+$degF = 98.6;
+$degC = ($degF - 32) / 9*5;
+echo $degC . '&degC';
